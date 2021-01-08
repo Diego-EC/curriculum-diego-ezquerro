4eb5f7431e40271a938975887aac85bb0e2863de
Diego <pasesinllamar@gmail.com>
2021-01-08 20:44:38 +0100

    saving progress

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/views/app';
 import Layout from "./layout";
-
 import './index.css';
+import Navbar from './js/components/bootstrap/navbar';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Navbar />
     <Layout>
       <App />
     </Layout>
