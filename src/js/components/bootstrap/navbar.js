7ac4e9e471f489bd355f46b8cf5512b34bb5cee6
Diego <diegoezquerro@gmail.com>
2021-01-14 20:06:23 +0100

    content added

--- src/js/components/bootstrap/navbar.js
+++ src/js/components/bootstrap/navbar.js
@@ -6,19 +6,19 @@ const Navbar = () => {
             <div className="" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item nav-link">
-                        <i className="fas fa-map-marker-alt"> Zaragoza, Spain</i>
+                        <i className="fas fa-map-marker-alt"></i> <span className="head-text-style"> Zaragoza, Spain</span>
                     </li>
                     <li className="nav-item nav-link">
-                        <i className="fas fa-envelope"> diego.ezquerro@gmail.com</i>
+                        <i className="fas fa-envelope"></i> <span className="head-text-style"> diego.ezquerro@gmail.com</span>
                     </li>
                     <li className="nav-item nav-link">
-                        <i className="fas fa-phone"> +34 625 848 735</i>
+                        <i className="fas fa-phone"></i> <span className="head-text-style"> +34 625 848 735</span>
                     </li>
                     <li className="nav-item nav-link">
-                        <a href="https://www.linkedin.com/in/diegoec/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"> https://www.linkedin.com/in/diegoec/</i></a>
+                        <a href="https://www.linkedin.com/in/diegoec/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i> <span className="head-text-style"> https://www.linkedin.com/in/diegoec/</span></a>
                     </li>
                     <li className="nav-item nav-link">                        
-                        <i className="fab fa-skype"> diego.ezquerro.calvo</i>
+                        <i className="fab fa-skype"></i> <span className="head-text-style"> diego.ezquerro.calvo</span>
                     </li>
                 </ul>
             </div>
@@ -30,7 +30,7 @@ const Navbar = () => {
         <div className="">
             <h1 className="">Diego Ezquerro Calvo</h1>
             <h3 className="">Junior Fullstack Developer</h3>
-            <div>{contactDetails}</div>
+            <div className="head-text-style">{contactDetails}</div>
             <h5 className="">Flip the cards two by two and for each pair you match you will unlock a part of my CV</h5>
         </div>
     </nav> );
--- src/js/components/resume.js
+++ src/js/components/resume.js
@@ -41,17 +41,6 @@ const Resume = forwardRef((props, ref) => {
         }
     }));
 
-    /*const personalDetails = (
-        <div>
-            <h3>Contact</h3>
-            linkedin: https://www.linkedin.com/in/diegoec/
-            email: diego.ezquerro@gmail.com
-            Phone: +34 625 848 735
-            Skype: diego.ezquerro.calvo
-            Lugar: Zaragoza, Spain
-        </div>
-    );*/
-
     const profile = (
         <div>
             <h3>Profile</h3>
@@ -62,47 +51,164 @@ const Resume = forwardRef((props, ref) => {
     const experience = (
         <div>
             <h3>Experience</h3>
-            <p>After more than ten years working in software development I can contribute with my experience to any kind of proyect. I have also finished recently a Fullstack Developer course in a programming Bootcamp and I have obtained knowledge in HTML5, CSS, Javascript, React, Python, Flask, SQL, Git, Bootstrap. I am looking for an opportunity that allows me to continue my development and grow in the programming sector.</p>
+            <table className="table table-borderless">
+                <thead>
+                    <tr>
+                        <th className="td-30" scope="col"></th>
+                        <th className="td-70" scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="">07/2018 – 07/2019</td>
+                        <td className="">UNITY DEVELOPER</td>
+                    </tr>
+                    <tr>
+                        <td>Pamplona, Spain</td>
+                        <td>iAR</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Development of Augmented Reality applications for mobile devices using the Unity engine, along with the Vuforia SDK and C#, consuming a REST API</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table className="table table-borderless">
+                <thead>
+                    <tr>
+                        <th className="td-30" scope="col"></th>
+                        <th className="td-70" scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="">09/2006 – 07/2018</td>
+                        <td className="">MAINFRAME DEVELOPER</td>
+                    </tr>
+                    <tr>
+                        <td>Madrid, Spain</td>
+                        <td>ATOS / ISBAN / ViewNext / FDS</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>More than ten years working for different clients (Santander Bank, Bank of Spain, Axa, Bankinter) using Mainframe technologies (COBOL, CICS, DBS, JCL) and in different types of projects (maintenance, new developments, mergins); assuming different responsibilities, from programmer to organic analyst.</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table className="table table-borderless">
+                <thead>
+                    <tr>
+                        <th className="td-30" scope="col"></th>
+                        <th className="td-70" scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="">03/2004 – 06/2005</td>
+                        <td className="">PROGRAMMER</td>
+                    </tr>
+                    <tr>
+                        <td>Zaragoza, Spain</td>
+                        <td>Pariver</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Desktop application programming using Visual Basic 6.5 and SQL Server.</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 
     const education = (
         <div>
             <h3>Education</h3>
-            <p>4Geeks Academy</p>
-            <p>IES Pablo Serrano</p>
+            <table className="table table-borderless">
+                <thead>
+                    <tr>
+                        <th className="td-30" scope="col"></th>
+                        <th className="td-70" scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td >08/2020 – 11/2020</td>
+                        <td >FULLSTACK DEVELOPER</td>
+                    </tr>
+                    <tr>
+                        <td>Remote</td>
+                        <td>4Geeks Academy</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Web development bootcamp where I've learned among other technologies: React, Bootstrap, Python, Flask, SQLAlchemy, Git, HTML5, CSS, JaveScript</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table className="table table-borderless">
+                <thead>
+                    <tr>
+                        <th className="td-30" scope="col"></th>
+                        <th className="td-70" scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td >09/2002 – 06/2004</td>
+                        <td >HIGHER NATIONAL DIPLOMA IN SOFTWARE DEVELOPMENT</td>
+                    </tr>
+                    <tr>
+                        <td>Zaragoza, Spain</td>
+                        <td>I.E.S. Pablo Serrano</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Development of computer applications, participating in designing, programming, testing and documentation. Manage databases, interpreting it's design and verifying it's integrity, consistency, security and accessibility of data</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 
     const skills = (
         <div>
             <h3>Skills</h3>
-            <p>4Geeks Academy</p>
-            <p>IES Pablo Serrano</p>
-            <p>4Geeks Academy</p>
-            <p>IES Pablo Serrano</p>
+            <p>
+                <span className="skill">React</span>
+                <span className="skill">Bootstrap</span>
+                <span className="skill">Python</span>
+                <span className="skill">HTML5</span>
+                <span className="skill">CSS</span>
+                <span className="skill">JavaScript</span>
+            </p>
+            <p>
+                <span className="skill">Flask</span>
+                <span className="skill">SQLAlchemy</span>
+                <span className="skill">Git</span>
+            </p>
         </div>
     );
 
     const languages = (
         <div>
             <h3>Languages</h3>
-            <p>4Geeks Academy</p>
-            <p>IES Pablo Serrano</p>
+            <span>English (F.C.E.)</span> <span>|</span> <span>Spanish (Native)</span>
         </div>
     );
 
     const softSkills = (
         <div>
             <h3>Soft Skills</h3>
-            <p>4Geeks Academy</p>
-            <p>IES Pablo Serrano</p>
+            <span className="skill">Creative</span>
+            <span className="skill">Insightful</span>
+            <span className="skill">Passionate</span>
         </div>
     );
 
     return ( 
         <div className="resume">
-            {/*<div className="personal-details">{personalDetails}</div>*/}
             <div className="profile" unlocked={unlockProfile}>{profile}</div>
             <div className="experience" unlocked={unlockExperience}>{experience}</div>
             <div className="education" unlocked={unlockEducation}>{education}</div>
